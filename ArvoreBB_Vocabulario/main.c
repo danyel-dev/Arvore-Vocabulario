7090a50b7c92d435ee576d0a01ccae2cc2a7f938
Carlos Daniel <danyelpinheiro154@hotmail.com>
2021-01-20 01:04:37 -0300

    update

--- ArvoreBB_Vocabulario/main.c
+++ ArvoreBB_Vocabulario/main.c
@@ -1,23 +1,106 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
-void divide_palavras(char frase[]);
+typedef struct listaIngles listaIngles;
+typedef struct Vocabulario Vocabulario;
 
-int main() {
+struct listaIngles {
+    char palavraEn[30];
+    listaIngles *prox;
+};
+
+struct Vocabulario {
+    char palavraPt[30];
+    listaIngles *significados;
+    Vocabulario *esq, *dir;
+};
+
+
+void divide_palavras(Vocabulario **Raiz, char frase[]);
+
+void insere_palavra(Vocabulario **Raiz, char p_portug[], char p_ingles[]);
+
+Vocabulario *aloca(char p_portug[], char p_ingles[]);
+
+listaIngles *insere_lista(listaIngles *significado, char p_ingles[]);
+
+void imprimeArv(Vocabulario *Raiz);
+
+void imprime_lista(listaIngles *significados);
+
+
+int main() {    
     char frase[1000]; 
-    FILE *fptr;
+    Vocabulario *Raiz = NULL;
 
+    FILE *fptr;
     fptr = fopen("vocabulario.txt", "r");
 
-    while(fgets(frase, 1000, fptr) != NULL) {
-        if (frase[0] != '%' && frase[3] != '%') {
-            divide_palavras(frase);
-        }
-    }
+    clock_t inicio, fim;
+    inicio = clock();
+
+    // inserção na árvore
+    while (fgets(frase, 1000, fptr) != NULL) 
+        if (frase[0] != '%' && frase[3] != '%') 
+            divide_palavras(&Raiz, frase);
+    
+    fim = clock();
+
+    printf("Tempo de inserção em nanosegundos: %d\n\n", (fim - inicio) * 1000000);
+
+    // impressão da árvore
+    imprimeArv(Raiz);
+    printf("\n");
+
+    // Busca na árvore
+    char palavra[30];
+    printf("Digite uma palavra para buscar: ");
+    scanf("%s", &palavra);
+    
+    inicio = clock();
+    int i = busca(Raiz, palavra);
+    fim = clock();
+
+    if (i == 0) 
+        printf("Palavra não encontrada\n");
+    
+    printf("\nTempo da busca em nanosegundos: %d\n", (fim - inicio) * 1000000);
+
+    // exclusão na árvore
+    printf("\nDigite uma palavra para excluir: ");
+    scanf("%s", &palavra);
+    
+    excluir_palavra(&Raiz, palavra);
+
+    // impressão da árvore
+    imprimeArv(Raiz);
+    printf("\n");
 
     return 0;
 }
 
-void divide_palavras(char frase[]) {
+int busca(Vocabulario *Raiz, char frase[]) {
+    int i = 0;
+
+    if (Raiz != NULL) {
+        int n = strcmp(frase, Raiz->palavraPt);
+        
+        if (n < 0)
+            busca((*Raiz).esq, frase);
+        else if (n > 0)
+            busca((*Raiz).dir, frase);
+        else {
+            printf("%s: ", (*Raiz).palavraPt);
+            imprime_lista((*Raiz).significados);
+            i = 1;
+        }
+    } else 
+        return i; 
+}
+
+void divide_palavras(Vocabulario **Raiz, char frase[]) {
     int cont = 0, i;
     char p_ingles[30], p_portug[30];
 
@@ -38,7 +121,7 @@ void divide_palavras(char frase[]) {
 
             p_portug[cont] = '\0';
             cont = 0;
-            printf("%s\n", p_portug);
+            insere_palavra(Raiz, p_portug, p_ingles);
         } 
 
         else {
@@ -46,5 +129,122 @@ void divide_palavras(char frase[]) {
             cont++;
         }
     }
+}
 
+Vocabulario *aloca(char p_portug[], char p_ingles[]) {
+    Vocabulario *No = (Vocabulario*) malloc(sizeof(Vocabulario));
+    strcpy((*No).palavraPt, p_portug);
+    (*No).esq = NULL;
+    (*No).dir = NULL;
+    (*No).significados = NULL;
+    (*No).significados = insere_lista((*No).significados, p_ingles);
+    
+    return No;
+}
+
+listaIngles *insere_lista(listaIngles *significados, char p_ingles[]) {
+    listaIngles *No = (listaIngles*) malloc(sizeof(listaIngles));
+    strcpy((*No).palavraEn, p_ingles);
+    (*No).prox = NULL;    
+    
+    if (significados == NULL) 
+        return No;
+    else {
+        listaIngles *aux = significados;
+
+        while (aux->prox != NULL) 
+            aux = aux->prox;
+
+        aux->prox = No;
+        return significados;
+    } 
+}
+
+void insere_palavra(Vocabulario **Raiz, char p_portug[], char p_ingles[]) {
+    if (*Raiz == NULL) {
+        *Raiz = aloca(p_portug, p_ingles);
+    } else {
+        int n = strcmp(p_portug, (**Raiz).palavraPt);
+
+        if (n < 0) 
+            insere_palavra(&(**Raiz).esq, p_portug, p_ingles); 
+
+        else if (n > 0)
+            insere_palavra(&(**Raiz).dir, p_portug, p_ingles);
+
+        else 
+            (**Raiz).significados = insere_lista((**Raiz).significados, p_ingles);
+    }
+}
+
+void imprimeArv(Vocabulario *Raiz) {
+    if (Raiz != NULL) {
+        printf("%s: ", Raiz->palavraPt);
+        imprime_lista((*Raiz).significados);
+        imprimeArv(Raiz->esq);
+        imprimeArv(Raiz->dir);
+    }
+}
+
+void imprime_lista(listaIngles *significados) {
+    for (listaIngles *aux = significados; aux != NULL; aux = aux->prox)
+        printf("%s ", aux->palavraEn);
+    printf("\n");
+}
+
+int qtd_filhos(Vocabulario *Raiz) {
+    if (Raiz->esq == NULL && Raiz->dir == NULL) 
+        return 0;
+    if (Raiz->esq != NULL && Raiz->dir != NULL) 
+        return 1;
+    if (Raiz->esq != NULL)
+        return 2;
+    else 
+        return 3;
+}
+
+Vocabulario *menorfilho(Vocabulario **Raiz) {
+    Vocabulario *menor;
+
+    if((**Raiz).esq != NULL)
+        menor = menorfilho(&(**Raiz).esq);
+    else 
+        menor = *Raiz;
+    
+    return menor;
+}
+
+void excluir_palavra(Vocabulario **Raiz, char palavra[]) {  
+    if (*Raiz != NULL) {
+        int n = strcmp(palavra, (**Raiz).palavraPt);
+
+        if (n < 0) 
+            excluir_palavra(&(**Raiz).esq, palavra);
+
+        else if (n > 0) 
+            excluir_palavra(&(**Raiz).dir, palavra);
+        
+        else {
+            Vocabulario *aux;
+            aux = *Raiz;
+
+            if (qtd_filhos(*Raiz) == 0)
+                *Raiz = NULL;
+
+            else if (qtd_filhos(*Raiz) == 2) 
+                *Raiz = (**Raiz).esq;
+
+            else if (qtd_filhos(*Raiz) == 3) 
+                *Raiz = (**Raiz).dir;
+
+            else if (qtd_filhos(*Raiz) == 1) {
+                Vocabulario *menor;
+                menor = menorfilho(&(**Raiz).dir);
+                menor->esq = (**Raiz).esq;
+                *Raiz = (**Raiz).dir;
+            }
+
+            free(aux);
+        }
+    } 
 }
---
+++ ArvoreBB_Vocabulario/teste.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int n;
+    n = strcmp("aa", "aazzzzzzzzzzzzzz");
+    printf("%d\n", n); 
+}
